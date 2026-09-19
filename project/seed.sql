8051d34fb0f19637388ea7039a6f59f451f5b25a
rikkipravalika <rikkipravalika07@gmail.com>
2026-09-19 08:48:02 +0530

    Delete project/seed.sql

--- project/seed.sql
+++
@@ -1,3 +0,0 @@
-USE expense_splitter;
--- Demo user can be created through the signup screen. Passwords are intentionally not stored here.
--- After signup, create a budget such as Shopping = ₹5,000 and add expenses to demonstrate automatic alerts.
